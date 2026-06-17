f8170d7652a65be605451343e29aec45d3bfa4ee
Jia Cheng Ouyang <jia@Jias-MacBook-Neo.local>
2026-06-17 10:38:22 -0700

    Add Python profile variables lab

--- profile.py
+++ profile.py
@@ -9,3 +9,5 @@ print(full_name, nick_name, age, used_python_before, hobbies, favorites)
 skills = ("Hardworking", "Leadership")
 languages_spoken = {"English", "Cantonese", "Mandarin", "Spanish"}
 print(skills, languages_spoken)
+
+# this buggy
