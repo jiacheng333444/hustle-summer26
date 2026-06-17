0dd60d69d4b0af3d60c3e9eedb7d2e264236dc7f
Jia Cheng Ouyang <jia@Jias-MacBook-Neo.local>
2026-06-17 10:35:52 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,10 @@
+full_name = "Jia Cheng Ouyang"
+nick_name = "Jia"
+age = 17
+used_python_before = False
+hobbies = ["Hiking", "Cooking", "Badminton", "Basketball"]
+favorites = {"movie": "Backrooms", "food": "everything", "hobby": "eating"}
+print(full_name, nick_name, age, used_python_before, hobbies, favorites)
+
+skills = ("Hardworking", "Leadership")
+languages_spoken = {"English", "Cantonese", "Mandarin", "Spanish"}
